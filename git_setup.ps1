19f7638209118558ead84c1da61a3afe7e657e8d
parasnath215 <parasnath215@github.com>
2026-03-22 20:20:24 +0530

    fix: use postgresql for render deployment and update start command

--- git_setup.ps1
+++ git_setup.ps1
@@ -1,7 +1,9 @@
 $git = "C:\Program Files\Git\cmd\git.exe"
-& $git init
-& $git add .
+& $git config user.email "parasnath215@github.com"
+& $git config user.name "parasnath215"
 & $git commit -m "Initialize project for Render deployment"
 & $git branch -M main
+# We may have already added the remote, so ignore errors on add and just set it
+& $git remote remove origin
 & $git remote add origin https://github.com/parasnath215/Swastik.git
 & $git push -u origin main
